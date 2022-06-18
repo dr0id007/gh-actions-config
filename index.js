59835ede990c9f216a3abd397a45fe59e47617ca
Dev Arora <nullvoid.nv@gmail.com>
2022-06-18 17:12:22 +0530

    feature route added (#1)

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
     return res.send("Hello World")
 })
 
+app.get('/feature', (req, res) => {
+    return res.send("Feature Test")
+})
+
 app.listen(3000 , () => console.log('listen in port:3000'))
 
